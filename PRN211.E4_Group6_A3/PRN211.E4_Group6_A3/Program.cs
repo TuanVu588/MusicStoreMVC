265490c3425d23ebc5483585395b17b8cee85326
TuanVu588 <80246371+TuanVu588@users.noreply.github.com>
2022-10-26 12:45:26 +0700

    index,privacy

--- PRN211.E4_Group6_A3/PRN211.E4_Group6_A3/Program.cs
+++ PRN211.E4_Group6_A3/PRN211.E4_Group6_A3/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PRN211.E4_Group6_A3.Models;
+
 namespace PRN211.E4_Group6_A3
 {
     public class Program
@@ -8,7 +11,11 @@ namespace PRN211.E4_Group6_A3
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            var conf = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
 
+            builder.Services.AddDbContext<MusicStoreContext>(option => option.UseSqlServer(conf.GetConnectionString("DbConnection")));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
