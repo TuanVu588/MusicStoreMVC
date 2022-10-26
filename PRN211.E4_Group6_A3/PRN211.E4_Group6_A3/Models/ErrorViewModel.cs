be7f1a759041cfdd92ffc840e2545fd4f008c615
TuanVu588 <80246371+TuanVu588@users.noreply.github.com>
2022-10-26 11:56:36 +0700

    first commit

---
+++ PRN211.E4_Group6_A3/PRN211.E4_Group6_A3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRN211.E4_Group6_A3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
