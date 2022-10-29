a5041b8d0ac699bf57c881eabafea27c3dd8b62f
TuanVu588 <80246371+TuanVu588@users.noreply.github.com>
2022-10-29 16:44:08 +0700

    login,logout

--- PRN211.E4_Group6_A3/PRN211.E4_Group6_A3/Controllers/HomeController.cs
+++ PRN211.E4_Group6_A3/PRN211.E4_Group6_A3/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.HttpSys;
 using PRN211.E4_Group6_A3.Models;
 using System.Diagnostics;
+
 
 namespace PRN211.E4_Group6_A3.Controllers
 {
@@ -22,7 +24,36 @@ namespace PRN211.E4_Group6_A3.Controllers
         {
             return View();
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            MusicStoreContext context = new MusicStoreContext();
+            User user1 = context.Users.Where(u => u.UserName == user.UserName && u.Password == user.Password).FirstOrDefault();
+            if(user1 == null)
+            {
+                return View();
+            }
+            else
+            {
+                HttpContext.Session.SetString("UserName", user.UserName);
+                HttpContext.Session.SetInt32("Role", user1.Role);
 
+                ShoppingCart cart = ShoppingCart.GetCart(HttpContext);
+                cart.MigrateCart(HttpContext);
+                return RedirectToAction("Index","Home");
+            }            
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            ShoppingCart cart = ShoppingCart.GetCart(HttpContext);
+            cart.EmptyCart();
+            return RedirectToAction("Index","Home");
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
