568e5d0c1c1cce7c9027e9aa6539262ee5640ff2
luannd123 <87166676+luannd123@users.noreply.github.com>
2022-10-30 22:20:12 +0700

    feat: Cart

---
+++ PRN211.E4_Group6_A3/PRN211.E4_Group6_A3/Controllers/ShoppingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PRN211.E4_Group6_A3.Models;
+
+namespace PRN211.E4_Group6_A3.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        MusicStoreContext context = new MusicStoreContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            
+
+            return View();
+        }
+    }
+}
